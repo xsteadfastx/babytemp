f70cbbe8eaef5bb5ba44eb51e402e8812c3669f5
Marvin Steadfast <marvin@xsteadfastx.org>
2017-02-21 10:49:15 +0100

    reset every night at 12pm

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ BABYTEMP
 ![babytemp interface](interface.png)
 
 1. `$ docker run -it --rm -p 1883:1883 -p 9001:9001 eclipse-mosquitto`
-2. `$ bokeh serve --show interface.py`
+2. Create `config.py`
+3. `make flash`
+4. `$ bokeh serve --show interface.py`
--- interface.py
+++ interface.py
@@ -17,7 +17,7 @@ import paho.mqtt.client as mqtt
 from tornado import gen
 
 
-p1 = figure(
+P1 = figure(
     title='Temperature',
     tools='pan',
     plot_width=600,
@@ -25,7 +25,7 @@ p1 = figure(
     x_axis_type='datetime'
 )
 
-p2 = figure(
+P2 = figure(
     title='Humidity',
     tools='pan',
     plot_width=600,
@@ -34,20 +34,20 @@ p2 = figure(
 )
 
 # data store
-temp_source = ColumnDataSource(data=dict(x=[], y=[]))
-humi_source = ColumnDataSource(data=dict(x=[], y=[]))
+TEMP_SOURCE = ColumnDataSource(data=dict(x=[], y=[]))
+HUMI_SOURCE = ColumnDataSource(data=dict(x=[], y=[]))
 
 # sensor values
-temp = None
-humi = None
+TEMP = None
+HUMI = None
 
 # define plots
-temp_graph = p1.line(x='x', y='y', source=temp_source, color="navy", alpha=0.5)
-humi_graph = p2.line(x='x', y='y', source=humi_source, color="red", alpha=0.5)
+TEMP_GRAPH = P1.line(x='x', y='y', source=TEMP_SOURCE, color="navy", alpha=0.5)
+HUMI_GRAPH = P2.line(x='x', y='y', source=HUMI_SOURCE, color="red", alpha=0.5)
 
 # This is important! Save curdoc() to make sure all threads
 # see then same document.
-doc = curdoc()
+DOC = curdoc()
 
 
 def on_connect(client, userdata, flags, rc):
@@ -55,54 +55,68 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    global temp
-    global humi
+    global TEMP
+    global HUMI
 
     if msg.topic == '/babytemp/temperature':
-        temp = float(msg.payload)
+        TEMP = float(msg.payload)
 
     if msg.topic == '/babytemp/humidity':
-        humi = float(msg.payload)
+        HUMI = float(msg.payload)
 
 
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
-mqtt_client.connect(MQTT_SERVER, 1883, 60)
+MQTT_CLIENT = mqtt.Client()
+MQTT_CLIENT.on_connect = on_connect
+MQTT_CLIENT.on_message = on_message
+MQTT_CLIENT.connect(MQTT_SERVER, 1883, 60)
 
 
 def mqtt_loop():
     while True:
         time.sleep(0.1)
-        mqtt_client.loop()
-        doc.add_next_tick_callback(partial(update))
+        MQTT_CLIENT.loop()
+        DOC.add_next_tick_callback(partial(update))
 
 
 @gen.coroutine
 def update():
-    if not temp or not humi:
+    if not TEMP or not HUMI:
         return
 
     now = datetime.now()
 
-    temp_source.stream(
-        {
-            'x': [now],
-            'y': [temp]
+    if now.hour == 0 and now.second == 0:
+
+        TEMP_SOURCE.data = {
+            'x': [],
+            'y': []
         }
-    )
 
-    humi_source.stream(
-        {
-            'x': [now],
-            'y': [humi]
+        HUMI_SOURCE.data = {
+            'x': [],
+            'y': []
         }
-    )
+
+    else:
+
+        TEMP_SOURCE.stream(
+            {
+                'x': [now],
+                'y': [TEMP]
+            }
+        )
+
+        HUMI_SOURCE.stream(
+            {
+                'x': [now],
+                'y': [HUMI]
+            }
+        )
 
 
-p = gridplot([p1], [p2], sizing_mode='stretch_both')
+P = gridplot([P1], [P2], sizing_mode='stretch_both')
 
-doc.add_root(p)
+DOC.add_root(P)
 
 thread = Thread(target=mqtt_loop)
 thread.start()
